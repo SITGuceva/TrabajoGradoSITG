eca1e7ec264dc10549516240443ccf403aa07b76
Alejandro Gómez <desarrollo@emporiocreativo.com>
2018-03-24 18:23:43 -0500

    24/03/2018 Alejandro
    Se implemento carusel para la ventana inicial

--- SITG/MenuPrincipal.aspx.cs
+++ SITG/MenuPrincipal.aspx.cs
@@ -1,11 +1,11 @@
-﻿
-using Oracle.DataAccess.Client;
+﻿using Oracle.DataAccess.Client;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -14,59 +14,82 @@ public partial class MenuPrincipal : Page
     Conexion con = new Conexion();
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Session["Usuario"] == null )
+        if (Session["Usuario"] == null)
         {
             Response.Redirect("Default.aspx");
         }
-        //Ftp();
+
+       /* EST.Visible = true;
+        ADM.Visible = false;
+        estudianterol.Visible = false;
+        profesorrol.Visible = false;
+        comiterol.Visible = false;
+        directorrol.Visible = false;*/
+
+        consultaroles();
     }
 
-    private void Ftp()
-    {
-        // Get the object used to communicate with the server.  
-        FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://192.168.1.7/"+FUdocumento.FileName);
-        request.Method = WebRequestMethods.Ftp.UploadFile;
 
-        // This example assumes the FTP site uses anonymous logon.  
-        request.Credentials = new NetworkCredential("ASUS VX199H", "cole1");
+    protected void consultaroles() {
+        
+        string rol = Session["rol"].ToString().Trim();
+        String[] ciclo = rol.Split(' ');
+        // label.Text = ciclo.Length+ "es";
+        // foreach (var cadena in ciclo)
+        string nombre = "";
+       for(int i=0; i<ciclo.Length; i++) {
+            //label.Text += ciclo[i] + "jaja";
 
-        // Copy the contents of the file to the request stream.  
-        StreamReader sourceStream = new StreamReader(FUdocumento.FileContent);
-        byte[] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
-        sourceStream.Close();
 
-        request.ContentLength = fileContents.Length;
-        Stream requestStream = request.GetRequestStream();
-        requestStream.Write(fileContents, 0, fileContents.Length);
-        requestStream.Close();
+            if (ciclo[i].Equals("EST"))
+            {
+                nombre = "Estudiante";
+            }
+            if (ciclo[i].Equals("DOC"))
+            {
+                nombre = "Docente";
+            }
+            if (ciclo[i].Equals("COM"))
+            {
+                nombre = "Comité";
+            }
+            if (ciclo[i].Equals("DIR"))
+            {
+                nombre = "Director";
+            }
+            if (ciclo[i].Equals("JUR"))
+            {
+                nombre = "Jurado";
+            }
+            if (ciclo[i].Equals("EVA"))
+            {
+                nombre = "Evaluador";
+            }
+            if (ciclo[i].Equals("DEC"))
+            {
+                nombre = "Decana";
+            }
+            Notificaciones.Controls.Add(new LiteralControl("<li style='background-color:black'><a href =\"#"+ciclo[i]+ "\" data-toggle=\"tab\" style=\"color:gray;\" > " +nombre+"</a></li>"));
+            
+            /*   if (cadena.Equals("EST"))
+               {
 
-        var response = (FtpWebResponse)request.GetResponse();
-        Linfo.Text = response.StatusDescription;
-        response.Close();
+                   estudianterol.Visible = true;
+               }
+               if (cadena.Equals("DOC"))
+               {
+                   profesorrol.Visible = true;
+               }
+
+               if (cadena.Equals("COM"))
+               {
+
+                   comiterol.Visible = true;
+               }*/
+        }
     }
 
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-        Ftp();
-    }
 
-    protected void Button2_Click(object sender, EventArgs e)
-    {
-        FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://192.168.1.7/primera.jpeg");
-        request.Method = WebRequestMethods.Ftp.DownloadFile;
 
-        // This example assumes the FTP site uses anonymous logon.  
-        request.Credentials = new NetworkCredential("ASUS VX199H", "cole1");
-
-        FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-
-        Stream responseStream = response.GetResponseStream();
-        StreamReader reader = new StreamReader(responseStream);
-        Console.WriteLine(reader.ReadToEnd());
-
-        Linfo.Text="Download Complete" +response.StatusDescription;
-
-        reader.Close();
-        response.Close();
-    }
 }
+
